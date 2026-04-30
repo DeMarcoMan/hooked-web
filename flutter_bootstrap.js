0348b51f170aeda79c0b66f7b684e45667ccc6fb
Marco van den Wildenberg <mvdwildenberg@gmail.com>
2026-04-30 10:46:36 +0200

    v2026.1

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"59aa584fdf100e6c78c785d8a5b565d1de4b48
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1680868719" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "581509792" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
